bd5ceb6fffc82f2fc3c466388e9b77961466bb4d
mehmet çalış <mehmet_cls@hotmail.com>
2020-06-04 23:25:45 +0300

    Add project files.

---
+++ RabbitMQExchangeType.Consumer/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQExchangeType.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://sejocreq:VgwWgwRr9uSpcKNWvHDetO8k2Fk7rj_I@fox.rmq.cloudamqp.com/sejocreq");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("logs", durable:true,type: ExchangeType.Fanout);
+
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queueName, exchange: "logs", "");
+
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, false);
+
+                    Console.WriteLine("Loglar bekleniyor...");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume(queueName, false, consumer);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var log = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        Console.WriteLine("log alındı: " + log);
+                        var time = int.Parse(GetMessage(args));
+                        Thread.Sleep(time);
+                        Console.WriteLine("loglama bitti...");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    };
+
+                    Console.WriteLine("Çıkış yapmak için tıklayınız...");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+        
+        private static string GetMessage(string[] args)
+        {
+            return args[0].ToString();
+        }
+    }
+}
---
+++ RabbitMQExchangeType.Publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQExchangeType.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://sejocreq:VgwWgwRr9uSpcKNWvHDetO8k2Fk7rj_I@fox.rmq.cloudamqp.com/sejocreq");
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("logs", durable: true, type: ExchangeType.Fanout);
+                    var message = GetMessage(args);
+
+                    for (int i = 1; i < 11; i++)
+                    {
+                        var bodyByte = Encoding.UTF8.GetBytes($"{message}-{i}");
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+
+                        channel.BasicPublish("logs", routingKey: "", properties, bodyByte);
+
+                        Console.WriteLine($"Mesajınız gönderilmiştir: {message}-{i}");
+                    }
+                }
+                Console.WriteLine("Çıkış yapmak için tıklanıyınız.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] arg)
+        {
+            return arg[0].ToString();
+        }
+    }
+}
